a0db26e46cb26410b32a00222e24932e2903948e
Nicola Sarzi Madidini <nicola_sarzus@libero.it>
2018-03-05 15:19:12 +0100

    Restore_db modified database name

--- restore_db.sh
+++ restore_db.sh
@@ -10,7 +10,7 @@ if [[ ! -z $CONTAINER && ! -z $SQLSCRIPT ]]; then
     echo "Copying SQL Script $SQLSCRIPT in container $CONTAINER"
     docker cp $SQLSCRIPT $CONTAINER:/db.sql
     echo "Running script in container $CONTAINER"
-    docker exec -it $CONTAINER sh -c "psql -U admin -W app < /db.sql"
+    docker exec -it $CONTAINER sh -c "psql -U admin -W application < /db.sql"
     echo "Done"
 else
     echo "Usage: $FILENAME <container_name> <sql_script>"
