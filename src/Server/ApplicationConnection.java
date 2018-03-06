e8b46c1d85de5839129b8d3c225db5ca19e33662
Fernando Di Lillo <f.dilillo@esconsulting.it>
2018-03-06 09:19:33 +0100

    remove unused import

--- src/Server/ApplicationConnection.java
+++ src/Server/ApplicationConnection.java
@@ -9,7 +9,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
