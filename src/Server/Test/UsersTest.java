e7c66f01fb65fb7d79c2783e08698f454b05e1f0
Fernando Di Lillo <f.dilillo@esconsulting.it>
2018-03-15 09:24:34 +0100

    modify user test for better testing

--- src/Server/Test/UsersTest.java
+++ src/Server/Test/UsersTest.java
@@ -2,33 +2,41 @@ package Server.Test;
 
 import Server.Entity.Users;
 import Server.Repository.UsersRepository;
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UsersTest {
+    private static String username = "Sansa";
+    private static String password = "Stark";
+    private static UsersRepository usersRepository = new UsersRepository();
+    private static Users user;
 
-    @Test
-    public void usersRepositoryTest() {
-        UsersRepository usersRepository = new UsersRepository();
-
-        assertNotEquals(null, usersRepository.read(), "la lettura deve essere una lista");
+    @BeforeAll
+    static void createUser() {
+        user = new Users(username, password);
+        user.save();
     }
 
-    @Test
-    public void usersEntityTest() {
-        UsersRepository usersRepository = new UsersRepository();
-        Users user = new Users("ale", "pela","wkpe@woef.it");
-        user.save();
-        Users user2 = usersRepository.getUserById(user.getId());
+    @Test void readUser() {
+        Users readUser = usersRepository.getUserById(user.getId());
+        String message = "errore di lettura";
 
-        String message = "errore dei filtri";
-        assertEquals(user.getId(), user2.getId(), message);
-        assertEquals(user.getId(), user2.getId(), message);
-        assertEquals(user.getId(), user2.getId(), message);
-        assertEquals(user.getId(), user2.getId(), message);
+        assertEquals(user.getUsername(), readUser.getUsername(), message);
+        assertEquals(user.getPassword(), readUser.getPassword(), message);
+        assertNull(readUser.getEmail(), message);
+    }
 
-        assertTrue(usersRepository.login(user.getUsername(),user.getPassword()), "Errore durante il login");
+    @Test void loginUser(){
+        String message = "Errore durante il login";
+
+        assertFalse(usersRepository.login("wrong username", "wrong password"), message);
+        assertFalse(usersRepository.login("wrong username", password), message);
+        assertFalse(usersRepository.login(username, "wrong password"), message);
+        assertTrue(usersRepository.login(username, password), message);
+    }
+
+    @AfterAll
+    static void deleteUser() {
         user.delete();
     }
 }
