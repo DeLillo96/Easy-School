915a78de55b5bc41e40284cc7f45bd17c367d78e
Fernando Di Lillo <f.dilillo@esconsulting.it>
2018-03-18 12:15:50 +0100

    added closable error notification

--- src/Client/Controller/Error.java
+++ src/Client/Controller/Error.java
@@ -1,30 +1,49 @@
 package Client.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Pane;
-
 import java.io.IOException;
 
-public class Error {
+public class Error implements EventHandler<ActionEvent> {
+    private Button errorButtonClose;
+    private Parent root;
+    private Scene mainScene;
+    private TextArea errorTextArea;
 
     public void render(String message, Scene mainScene) {
-        Parent root = null;
         try {
             root = FXMLLoader.load(getClass().getResource("../Views/error.fxml"));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        TextArea errorTextArea = (TextArea) root.lookup("#errorTextArea");
+        errorTextArea = (TextArea) root.lookup("#errorTextArea");
         errorTextArea.setText(message);
-        errorTextArea.setWrapText(true);
 
-        root.setOpacity(0.85);
+        errorButtonClose = (Button) root.lookup("#errorButtonClose");
+        errorButtonClose.setOnAction(this);
 
+        this.mainScene = mainScene;
         Pane mainRoot = (Pane) mainScene.getRoot();
         mainRoot.getChildren().add(root);
     }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if(event.getSource() == errorButtonClose) {
+            removeErrorsPane();
+        }
+    }
+
+    private void removeErrorsPane() {
+        Pane mainRoot = (Pane) mainScene.getRoot();
+        mainRoot.getChildren().remove(root);
+    }
+
 }
