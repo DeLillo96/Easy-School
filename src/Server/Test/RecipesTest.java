eaa9dea792184960c78b23c20c781974c545dab3
Nicola Sarzi Madidini <nicola_sarzus@libero.it>
2018-03-20 14:59:15 +0100

    Recipes Test

---
+++ src/Server/Test/RecipesTest.java
@@ -0,0 +1,51 @@
+package Server.Test;
+
+import Server.Entity.Alimento;
+import Server.Entity.Dish;
+import Server.Repository.DishRepository;
+import Server.Result;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RecipesTest {
+
+    private static Dish dish = new Dish("PaneLupo", "Primo");
+    private static Alimento firstAlim = new Alimento("Pane");
+    private static Alimento secondAlim = new Alimento("Lupo");
+    private DishRepository dishRepository = new DishRepository();
+
+    @BeforeAll
+    static void createNewRecipe() {
+        firstAlim.save();
+        secondAlim.save();
+        dish.getIngredients().add(firstAlim);
+        dish.save();
+    }
+
+    @Test
+    void readRecipe() {
+        Dish readDish = dishRepository.getDishByName(dish.getName());
+
+        assertNotNull(readDish, "Read recipe error");
+        assertNotNull(readDish.getIngredients(), "Failed join");
+    }
+
+    @Test
+    void addNewIngredient() {
+        assertTrue(dish.getIngredients().add(secondAlim), "Il PaneLupo si fa solo con il pane");
+
+        Result result = dish.save();
+        assertTrue(result.isSuccess(), "Errore in salvataggio " + result.getMessages().toString());
+    }
+
+    @AfterAll
+    static void deleteParents() {
+        dish.delete();
+        firstAlim.delete();
+        secondAlim.delete();
+    }
+}
