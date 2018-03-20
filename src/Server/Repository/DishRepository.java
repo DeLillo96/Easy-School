1764066604c183f90a2c340ef543abd233a4959b
Nicola Sarzi Madidini <nicola_sarzus@libero.it>
2018-03-20 14:58:42 +0100

    Dish Repository

---
+++ src/Server/Repository/DishRepository.java
@@ -0,0 +1,35 @@
+package Server.Repository;
+
+import Server.Entity.Dish;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class DishRepository extends AbstractRepository {
+
+    public DishRepository() {super("Dish"); }
+
+    public Dish getDishById(Integer id) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("id", id);
+        List dishes = read(params);
+
+        return dishes != null && dishes.size() == 1 ? (Dish) dishes.get(0) : null;
+    }
+
+    public Dish getDishByName(String name) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("name", name);
+        List dishes = read(params);
+
+        return dishes != null && dishes.size() == 1 ? (Dish) dishes.get(0) : null;
+    }
+
+    public Dish getDishByCategory(String category) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("category", category);
+        List dishes = read(params);
+
+        return dishes != null && dishes.size() == 1 ? (Dish) dishes.get(0) : null;
+    }
+}
